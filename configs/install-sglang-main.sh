3caf7825b841203be2b4f8a138152f558a5d31a8
ishandhanani <ishandhanani@gmail.com>
2025-12-29 17:34:37 -0600

    patch

--- configs/install-sglang-main.sh
+++ configs/install-sglang-main.sh
@@ -12,3 +12,6 @@ cd sglang
 git config --global --add safe.directory "*"
 pip install -e "python"
 
+python3 -m pip install nvidia-nccl-cu12==2.28.3 --force-reinstall --no-deps
+python3 -m pip install nvidia-cudnn-cu12==9.16.0.29 --force-reinstall --no-deps
+python3 -m pip install nvidia-cutlass-dsl==4.3.0 --force-reinstall --no-deps
