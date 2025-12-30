da316514307d61fad413258753bed3052cef1ee4
ishandhanani <ishandhanani@gmail.com>
2025-12-29 23:42:11 -0600

    anotehr one?

--- configs/install-sglang-main.sh
+++ configs/install-sglang-main.sh
@@ -12,4 +12,4 @@ cd sglang
 git config --global --add safe.directory "*"
 pip install -e "python"
 
-pip install nvidia-nccl-cu12==2.28.3 nvidia-cudnn-cu12==9.16.0.29 nvidia-cutlass-dsl==4.3.0 --force-reinstall --no-deps
+#pip install nvidia-nccl-cu12==2.28.3 nvidia-cudnn-cu12==9.16.0.29 nvidia-cutlass-dsl==4.3.0 --force-reinstall --no-deps
